9553849c639fb86378a933bf649af68330565786
MuhammadNuman24 <numanrazzaq24@gmail.com>
2022-07-07 06:42:25 +0000

    Logo and theme Setup

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,5 +6,18 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 export default createVuetify(
+  {
+    theme: {
+      themes: {
+        light: {
+          lightGreen: '#009688',
+          Grey: '#E9E9E9',
+          lightGrey: '#E2E5ED',
+          DarkGrey: '#A8A8A8',
+          background: '#F2F3F7'
+        },
+      },
+    },
+  }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 )
